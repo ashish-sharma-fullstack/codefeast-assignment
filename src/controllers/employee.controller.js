5b8d74b37bb2080ccab62e4a7036a103ed5716b2
Ashish Sharma <ashish.sharma.php@gmail.com>
2026-05-03 12:00:50 +0530

    refactor(employee): reuse validation for update endpoint

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -26,6 +26,7 @@ const findById = async (req, res, next) => {
 const update = async (req, res, next) => {
   try {
     const employee = await employeeService.update(req.params.id, req.body);
+
     res.status(200).json({ success: true, data: employee });
   } catch (err) { next(err); }
 };
